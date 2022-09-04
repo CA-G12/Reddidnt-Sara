947d075000c7648fe73bbaa1cd45a368cc893685
unknown <saritadh74@gmail.com>
2022-09-04 20:19:34 +0300

    created the validatin scehmas for sign in and sign up relates #3

---
+++ server/validation/signUpValidation.js
@@ -0,0 +1,25 @@
+const Joi = require('joi');
+
+const signUpValidation = Joi.object({
+  name: Joi
+    .string()
+    .alphanum()
+    .min(3)
+    .max(50)
+    .required(),
+  email: Joi
+    .string()
+    .min(8)
+    .max(100)
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+    .required(),
+  password: Joi
+    .string()
+    .alphanum()
+    .required(),
+  repeat_password: Joi
+    .ref('password')
+    .required(),
+});
+
+module.exports = signUpValidation;
