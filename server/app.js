f67f152c38a15333d2d13c445ed2db2ce8753faa
unknown <saritadh74@gmail.com>
2022-09-05 21:11:25 +0300

    madethe authentication middleware, which tests where the user is authenticated or not
    relates #15

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const compression = require('compression');
 const cookieParser = require('cookie-parser');
 const router = require('./routers');
+const authenticate = require('./middleware/authentication')
 
 const app = express();
 
@@ -13,8 +14,15 @@ app.use(cookieParser());
 app.disable('x-powered-by');
 
 app.set('port', process.env.PORT || 3000);
+
+// app.use(authenticate);
+
 app.use(express.static(join(__dirname, '..', 'public')));
 app.use(router);
+
+app.use((req, res, next) => {
+  res.status(404).send('not found');
+});
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500).json({ message: err.msg || 'something went wrong' });
