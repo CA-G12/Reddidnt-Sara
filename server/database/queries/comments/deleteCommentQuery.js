c09fd1abef60c8811c1416fec5d6f0ab23b30db3
unknown <saritadh74@gmail.com>
2022-09-06 03:16:07 +0300

    added the queries responsible for adding and deleting comments relates #17

--- server/database/queries/comments/deleteCommentQuery.js
+++ server/database/queries/comments/deleteCommentQuery.js
@@ -1,5 +1,6 @@
 const connection = require('../../config/connection');
 
-const deleteCommentQuery = () => {};
+const deleteCommentQuery = ({ comId }) => connection
+  .query('DELETE FROM comments WHERE id = ($1)', [comId]);
 
 module.exports = deleteCommentQuery;
