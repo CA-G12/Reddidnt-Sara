b72840df9445bbf4d76426461a3c15fefe236ca7
unknown <saritadh74@gmail.com>
2022-09-07 20:53:55 +0300

    handled the dom for thehome page posts relates #24

--- public/js/index.js
+++ public/js/index.js
@@ -37,6 +37,65 @@ switchSignUp.addEventListener('click', displaySignIn);
 
 const logButtons = document.querySelector('header .log-in');
 const userInfo = document.querySelector('header .user-info');
+const postsContainer = document.querySelector('.posts');
+
+const handleHomePage = (data) => {
+  postsContainer.textContent = '';
+  data.forEach((e, i) => {
+    console.log(e);
+    const post = document.createElement('div');
+    const userData = document.createElement('div');
+    const content = document.createElement('div');
+    const interactions = document.createElement('div');
+    const likesCon = document.createElement('div');
+    const commentsCon = document.createElement('div');
+
+    const userImg = document.createElement('img');
+    let postImg = null;
+    if (e.post_img) {
+      postImg = document.createElement('img');
+      postImg.src = e.post_img;
+    }
+    const userName = document.createElement('h3');
+    const date = document.createElement('h4');
+    const postTitle = document.createElement('h2');
+
+    const postContent = document.createElement('p');
+
+    const likesIcon = document.createElement('i');
+    const commentsIcon =document.createElement('i');
+
+    const likesNum = document.createElement('span');
+    const commentsNum = document.createElement('span');
+
+    post.id = e.id;
+    post.classList = 'post';
+    userData.classList = 'user';
+    content.classList = 'content';
+    interactions.classList = 'interactions';
+    likesIcon.classList = 'fa-solid fa-heart';
+    commentsIcon.classList = 'fa-solid fa-comment';
+
+    userImg.src = e.user_img;
+    // postImg.src = e.post_img || '';
+    userName.textContent = e.name;
+    date.textContent = e.date.split('T')[0];
+    postTitle.textContent = 'title will be added to db later';
+    postContent.textContent = e.post;
+    likesNum.textContent = `${e.likes} Likes`;
+    commentsNum.textContent = 'Comments';
+
+    postsContainer.appendChild(post);
+    post.append(userData, content, interactions);
+    userData.append(userImg, userName, date);
+    if (postImg) {
+      content.append(postTitle, postContent, postImg);
+    } else { content.append(postTitle, postContent); }
+    likesCon.append(likesIcon, likesNum);
+    commentsCon.append(commentsIcon, commentsNum);
+    interactions.append(likesCon, commentsCon);
+  });
+};
 
 const fetchHomepageData = () => {
   fetch('/users/homepage')
@@ -47,6 +106,7 @@ const fetchHomepageData = () => {
         logButtons.style.display = 'none';
         userInfo.children[0].src = res.user.user_img;
         userInfo.children[1].textContent = res.user.name;
+        handleHomePage(res.data);
       }
     });
 };
